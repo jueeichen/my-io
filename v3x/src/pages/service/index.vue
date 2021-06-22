294873ec6f0f583a26f4fad730974000c6886f76
jueeiwin <290174020@qq.com>
2021-06-23 00:23:16 +0800

    1

--- v3x/src/pages/service/index.vue
+++ v3x/src/pages/service/index.vue
@@ -1,12 +1,32 @@
 <template>
-  <navbar :parameter="parameter" />
-  <view class="about">
-
+  <view class="hide-100vh">
+    <navbar :parameter="parameter" />
+    <view class="service">
+      <!-- <view > </view> -->
+      <view class="service-container">
+        <view class="service-container-top">
+          <image src=""></image>
+          <view>
+            <view>静音 </view>
+            <view>广东 深圳</view>
+          </view>
+        </view>
+        <image class="qr-code" src=""></image>
+        <view class="service-content-1"> 班主任微信</view>
+        <view class="service-content-2"> 扫一扫上面的二维码, 加我微信</view>
+      </view>
+      <view class="service-content-3"
+        >请保存到相册后，微信扫一扫加老师微信号
+      </view>
+      <view class="service-btn">
+        <image src="" />
+        <view> 保存图片</view>
+      </view>
+    </view>
   </view>
 </template>
 
 <script>
-
 import navbar from "@/components/navbar/index.vue";
 
 import "./index.styl";
@@ -15,7 +35,7 @@ import { useStore } from "vuex";
 export default {
   name: "about",
   components: {
-    navbar
+    navbar,
   },
   setup(props) {
     const store = useStore();
@@ -27,10 +47,10 @@ export default {
       test,
       parameter: {
         title: "班主任微信",
-        return:1
+        return: 1,
       },
     };
-  }
+  },
 };
 </script>
 
